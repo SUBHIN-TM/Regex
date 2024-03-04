b52ef1c2419c7655f29814705cf26f9e21441f9b
SUBHIN-TM <subhintm2018@gmail.com>
2024-03-04 09:09:39 +0530

    added

--- regex.js
+++ regex.js
@@ -89,3 +89,18 @@ function order(words) {
  }
  console.log( "Qstn",order("is2 Thi1s T4est 3a"));
  
+
+
+//  ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// If the function is passed a valid PIN string, return true, else return false.
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN (pin) {
+  let regex = /^(\d{4}|\d{6})$/ ;
+   return regex.test(pin)
+ }
+
+ console.log(validatePIN("1234"));
